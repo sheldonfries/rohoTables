a8202d02d9a5c6cf86030d82cb865e978bc79c50
Connor Robertson <connortprobertson@gmail.com>
2020-03-23 22:29:23 -0700

    test

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,16 @@
 // Update with your config settings.
 console.log({
-  database: process.env.DATABASE_IP,
-  port: process.env.DATABASE_PORT,
-  user: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD
+  client: 'postgresql',
+  connection: process.env.DATABASE_URL,
+  searchPath: ['knex', 'public'],
+
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  }
 });
 module.exports = {
   development: {
@@ -33,12 +40,9 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: process.env.DATABASE_IP,
-      port: process.env.DATABASE_PORT,
-      user: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD
-    },
+    connection: process.env.DATABASE_URL,
+    searchPath: ['knex', 'public'],
+
     pool: {
       min: 2,
       max: 10
