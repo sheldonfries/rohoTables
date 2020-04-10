e225bcf06271caab72de9023aea0c6844c2e27c6
Connor Robertson <connortprobertson@gmail.com>
2020-04-10 16:45:49 -0700

    added goalie total row

--- api/sql/getPlayer.js
+++ api/sql/getPlayer.js
@@ -1,16 +1,29 @@
 module.exports = {
   getPlayerStatsSql: `
-SELECT p.id,IFNULL(ps.pos,p.pos ) as pos, ps.season_id , 
+  SELECT p.id,IFNULL(ps.pos,p.pos ) as pos, ps.season_id , 
 se.season ,
 ps.season_type, 
 ps.Team AS team_name, 
-ps.gp , ps.g , 
-ps.a , ps.\`+/-\` , 
-ps.pim, ps.ppg ,ps.shp ,
-ps.ht ,ps.gwg , ps.sog, 
-ROUND(ps.g/IFNULL(ps.SOG,999999)* 100) AS shot_percent, 
+ps.gp , 
+ps.g , 
+ps.a , 
+ps.\`+/-\` , 
+ps.pim, 
+ps.ppg ,
+ps.ppp,
+ps.shp ,
+ps.ht ,
+ps.ga,
+ps.ta,
+ps.p,
+ps.atoi,
+ps.appt,
+ps.apkt,
+ps.gwg , 
+ps.sog, 
+CONVERT(ROUND(ps.g/IFNULL(ps.SOG,999999)* 100), CHAR) AS shot_percent, 
 ps.sb ,
-ROUND(ps.FO *100) AS faceoff_percent
+CONVERT(ROUND(ps.FO *100),CHAR) AS faceoff_percent
 FROM players AS p
 LEFT JOIN teams t
 ON t.id = p.team_id 
@@ -19,11 +32,11 @@ ON p.name =ps.Name
 LEFT JOIN seasons AS se 
 ON se.id =ps.season_id 
 WHERE p.name ='!!{playerName}!!'
-ORDER BY se.id
+ORDER BY se.id DESC
     `,
   getGoalieStatsSql: `
     SELECT p.id, gs.season_id , 
-se.Season , 
+se.season , 
 gs.season_type , 
 gs.Team as team_name, 
 gs.gp,
--- client/src/components/PlayerStatsTable.js
+++ client/src/components/PlayerStatsTable.js
@@ -4,37 +4,38 @@ import MaterialTable from 'material-table';
 
 const playerCols = [
   { title: 'Season', field: 'season' },
-  {
-    title: 'Team',
-    field: 'team_name',
-  },
-  { title: 'GP', field: 'gp' },
+  { title: 'Team', field: 'team_name' },
+  { title: 'GP', field: 'gp', type: 'numeric' },
   { title: 'G', field: 'g', type: 'numeric' },
   { title: 'A', field: 'a', type: 'numeric' },
   { title: 'P', field: 'p', type: 'numeric' },
-  { title: '+/-', field: '+/-' },
+  { title: '+/-', field: '+/-', type: 'numeric' },
   { title: 'PIM', field: 'pim', type: 'numeric' },
-  { title: 'PPG', field: 'ppg', type: 'numeric' },
+  { title: 'PPP', field: 'ppp', type: 'numeric' },
   { title: 'SHp', field: 'shp', type: 'numeric' },
   { title: 'Ht', field: 'ht', type: 'numeric' },
-  { title: 'GWG', field: 'gwg', type: 'numeric' },
-  { title: 'S', field: 's', type: 'numeric' },
+  { title: 'GA', field: 'ga', type: 'numeric' },
+  { title: 'TA', field: 'ta', type: 'numeric' },
+  { title: 'SOG', field: 'sog', type: 'numeric' },
   { title: 'S%', field: 'shot_percent', type: 'numeric' },
   { title: 'SB', field: 'sb', type: 'numeric' },
-  { title: 'TOI/GP', field: 'time_by_goals', type: 'numeric' },
+  { title: 'ATOI', field: 'atoi', type: 'time' },
+  { title: 'APPT', field: 'appt', type: 'time' },
+  { title: 'APKT', field: 'apkt', type: 'time' },
   { title: 'FO%', field: 'faceoff_percent', type: 'numeric' },
 ];
+
 const goalieCols = [
   { title: 'Season', field: 'season' },
   { title: 'Team', field: 'team_name' },
-  { title: 'GP', field: 'gp' },
+  { title: 'GP', field: 'gp', type: 'numeric' },
   { title: 'W', field: 'w', type: 'numeric' },
   { title: 'L', field: 'l', type: 'numeric' },
   { title: 'T', field: 't', type: 'numeric' },
-  { title: 'SHA', field: 'sha' },
+  { title: 'SHA', field: 'sha', type: 'numeric' },
   { title: 'GA', field: 'ga', type: 'numeric' },
   { title: 'GAA', field: 'gaa', type: 'numeric' },
-  { title: 'SV', field: 'sv', type: 'numeric' },
+  { title: 'SV%', field: 'sv', type: 'numeric' },
   { title: 'SO', field: 'so', type: 'numeric' },
   { title: 'G', field: 'g', type: 'numeric' },
   { title: 'A', field: 'a', type: 'numeric' },
@@ -46,6 +47,57 @@ export default function PlayersTable(props) {
   const { stats, title, pos } = props;
 
   if (stats.length === 0) return null;
+  // get total row
+
+  const totalRow = stats.reduce((totals, stat) => {
+    if (pos === 'G') {
+      goalieCols.forEach((col) => {
+        if (col.type === 'numeric') {
+          totals[col.field] =
+            typeof totals[col.field] === 'number'
+              ? totals[col.field] + parseFloat(stat[col.field])
+              : parseFloat(stat[col.field]);
+        }
+      });
+    } else {
+      playerCols.forEach((col) => {
+        if (col.type === 'numeric') {
+          totals[col.field] =
+            typeof totals[col.field] === 'number'
+              ? totals[col.field] + parseFloat(stat[col.field])
+              : parseFloat(stat[col.field]);
+        } else if (col.type === 'time') {
+          totals[col.field] =
+            typeof totals[col.field] === 'string'
+              ? sumTimeString(totals[col.field], stat[col.field])
+              : stat[col.field];
+        }
+      });
+    }
+    return totals;
+  }, {});
+
+  if (pos === 'G') {
+    totalRow.gaa =
+      totalRow.ga && totalRow.toi
+        ? ((totalRow.ga * 60) / totalRow.toi).toFixed(2)
+        : 0;
+    totalRow.sv =
+      totalRow.ga && totalRow.sha
+        ? (1 - totalRow.ga / totalRow.sha).toFixed(3)
+        : 0;
+  } else {
+    totalRow.shot_percent =
+      totalRow.sog && totalRow.g
+        ? Math.round((totalRow.g / totalRow.sog) * 100)
+        : 0;
+    totalRow.faceoff_percent = totalRow.faceoff_percent / stats.length;
+    totalRow.atoi = divideTimeString(totalRow.atoi, stats.length);
+    totalRow.appt = divideTimeString(totalRow.appt, stats.length);
+    totalRow.apkt = divideTimeString(totalRow.apkt, stats.length);
+  }
+  totalRow.season = 'Total';
+  stats.push(totalRow);
   return (
     <div>
       <div style={{ maxWidth: '100%' }}>
@@ -57,6 +109,31 @@ export default function PlayersTable(props) {
       </div>
     </div>
   );
+}
+
+function sumTimeString(time1, time2) {
+  const time1Mins =
+    parseInt(time1.split(':')[0]) * 60 + parseInt(time1.split(':')[1]);
+  const time2Mins =
+    parseInt(time2.split(':')[0]) * 60 + parseInt(time2.split(':')[1]);
+
+  const grandTotalMins = time1Mins + time2Mins;
+
+  //Now using your own code
+
+  var bookH = `0${Math.floor(grandTotalMins / 60)}`;
+  var bookM = `0${grandTotalMins % 60}`;
+
+  return bookH.slice(1) + ':' + bookM.slice(1);
+}
+
+function divideTimeString(time, divisor) {
+  const totalMins =
+    parseInt(time.split(':')[0]) * 60 + parseInt(time.split(':')[1]);
+  const dividedMins = totalMins / divisor;
+  var bookH = `0${Math.floor(dividedMins / 60)}`;
+  var bookM = `0${Math.round(dividedMins % 60)}`;
+  return bookH.slice(1) + ':' + bookM.slice(1);
 }
 // country: "ca"
 // name: "Ray Whitney"
