6e98e5a3ad51680f0e169eae698dda47bfe975ef
conconrob <connortprobertson@gmail.com>
2021-08-20 22:22:25 -0700

    feat: add procfile

--- server.js
+++ server.js
@@ -2,17 +2,17 @@ const express = require('express');
 const helmet = require('helmet');
 const path = require('path');
 const cors = require('cors');
-const csv = require('csv-string');
 const api = require('./api/index');
-const db = require('./db');
 
 const server = express();
+
 server.use(helmet());
 server.use(cors());
 server.use(express.json({ limit: '50mb' }));
 server.use('/api', api);
 
 server.use((req, res, next) => {
+  console.log('here');
   if (req.path.indexOf('.') === -1) {
     req.url += '.html';
   }
