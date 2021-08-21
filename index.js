b4f808a16625e767165ca636cc5c40b3a759ca71
conconrob <connortprobertson@gmail.com>
2021-08-20 22:21:38 -0700

    feat: heroku procfile

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const port = process.env.PORT || 6000;
 
 const server = require('./server');
+
 server.listen(port, () =>
   console.log(`\n** server running on http://localhost:${port} **\n`)
 );
