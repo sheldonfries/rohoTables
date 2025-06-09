03c6df498ee7598b679e3c40e7aa3940ae245cef
Sheldon Fries <sheldonfries@gmail.com>
2025-06-09 18:03:50 -0400

    chore: added dev configs

--- client/src/requester.js
+++ client/src/requester.js
@@ -4,7 +4,8 @@ import Axios from 'axios';
 
 const isDev = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
 
-const APIBASE = isDev ? '' : 'http://146.235.205.152:5000';
+// const APIBASE = isDev ? '' : 'http://146.235.205.152:5000';
+const APIBASE = isDev ? '' : 'http://localhost:5000';
 
 const requester = Axios.create({ baseURL: APIBASE });
 
